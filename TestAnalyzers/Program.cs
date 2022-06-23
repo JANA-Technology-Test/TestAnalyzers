354c92c30a7fad372d1bcf4f22fa5ee9056d277b
Ashley Ee <ee@janacorporation.com>
2022-06-23 10:21:17 -0400

    Switch to old program style

--- TestAnalyzers/Program.cs
+++ TestAnalyzers/Program.cs
@@ -1,18 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
 using TestAnalyzers;
 
-Console.WriteLine("Hello, World!");
+namespace TestAnalyzers
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
 
-TestClass c = new TestClass();
-c.DoSomething("hellO");
-string s = null;
-c.DoSomething(s);
-var results = await c.DoQueryAsync("1=1", "connection string...");
+            TestClass c = new TestClass();
+            c.DoSomething("hellO");
+            string s = null;
+            c.DoSomething(s);
+            var results = await c.DoQueryAsync("1=1", "connection string...");
 
-foreach (var result in results)
-    Console.WriteLine(result);
+            foreach (var result in results)
+                Console.WriteLine(result);
 
-var results2 = await c.DoAnotherQueryAsync("ness", "pk123", "connection string");
+            var results2 = await c.DoAnotherQueryAsync("ness", "pk123", "connection string");
 
-RSPEC3649SQLiNoncompliant r = new RSPEC3649SQLiNoncompliant(new DapperContext(new System.Data.SqlClient.SqlConnection("connection string")));
-r.Authenticate("mY User");
+            RSPEC3649SQLiNoncompliant r = new RSPEC3649SQLiNoncompliant(new DapperContext(new System.Data.SqlClient.SqlConnection("connection string")));
+            r.Authenticate("mY User");
+        }
+    }
+}
